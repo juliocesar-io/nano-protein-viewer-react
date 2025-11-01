6af2623b530d891dd924dfcd829c1c98a360590e
Julio César <jc@juliocesar.io>
2025-11-01 18:11:12 -0500

    fix packaing issues

--- src/components/NanoProteinViewer.tsx
+++ src/components/NanoProteinViewer.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import '../molstar.css';
 import type { LoadedStructure, StructureFormat, StructureUrl } from '@types';
